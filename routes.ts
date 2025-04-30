efadcd44e8e58a638d8143933f2e36898d724607
youhetest <156542005@qq.com>
2025-04-30 17:39:36 +0800

    Update routes.ts

--- routes.ts
+++ routes.ts
@@ -1,4 +1,4 @@
-import { Router, Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { Router, Application, Context /* etc. */ } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import * as dejs from "dejs";
 import * as mockService from "./mockService.ts";
 import * as path from "std/path/mod.ts";
