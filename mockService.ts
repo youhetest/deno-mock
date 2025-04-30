e35385914ebadab8b25691d78ff5dcdedcaea7f0
youhetest <156542005@qq.com>
2025-04-30 15:16:50 +0800

    Update mockService.ts

--- mockService.ts
+++ mockService.ts
@@ -1,4 +1,13 @@
-import * as path from "std/path/mod.ts";
+// Instead of: import { Application } from "oak";
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+
+// Instead of: import * as dejs from "dejs";
+import * as dejs from "https://deno.land/x/dejs@0.10.3/mod.ts";
+
+// Instead of: import * as path from "std/path/mod.ts";
+import * as path from "https://deno.land/std@0.208.0/path/mod.ts";
+
+// import * as path from "std/path/mod.ts";
 import { exists } from "std/fs/exists.ts";
 
 const MOCKS_FILE = path.join(Deno.cwd(), "mocks.json");
