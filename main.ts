b417f35d2c03eff6d6ad0ac488737ed837d92e44
youhetest <156542005@qq.com>
2025-04-30 15:16:10 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
+// Instead of: import { Application } from "oak";
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+
+// Instead of: import * as dejs from "dejs";
+import * as dejs from "https://deno.land/x/dejs@0.10.3/mod.ts";
+
+// Instead of: import * as path from "std/path/mod.ts";
+import * as path from "https://deno.land/std@0.208.0/path/mod.ts";
+
 import { Application } from "oak";
-import router from "./routes.ts";
-import { loadMocks } from "./mockService.ts";
+import router from "routes.ts";
+import { loadMocks } from "mockService.ts";
 
 const app = new Application();
-const port = 8000; // Or use Deno.env.get("PORT")
+const port = 8055; // Or use Deno.env.get("PORT")
 
 // Logger middleware (optional)
 app.use(async (ctx, next) => {
